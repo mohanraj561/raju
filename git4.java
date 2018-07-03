08fddc8fc772b9daefca6fc44b29a6c4e4b195b2
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-03 06:18:33 +0530

    Create git4.java

---
+++ git4.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class git4
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.print(a+".");
+        
+    }
+}
