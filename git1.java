e6a6f25f66555753afb4c74c3b22a40488c5923f
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-03 05:34:17 +0530

    Create git1.java

---
+++ git1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class git1
+{ 
+public static void main(String args[]) 
+{ 
+Scanner sc=new Scanner(System.in); 
+String str=sc.nextLine(); 
+StringBuffer str1=new StringBuffer(str); 
+String str2=str1.reverse().toString(); 
+System.out.print(str2); 
+}
+}
