86bb57164b248fed292fa84966ba518080829d63
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-08 21:09:42 +0530

    Create his.java

---
+++ his.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class his 
+{
+public static void main(String[] args)
+	{
+	 Scanner sc=new Scanner(System.in);
+	 int i=sc.nextInt();
+	 if(i%2==0)
+	 {
+		 System.out.print("Even");
+	 }
+	 else if(i%2==1)
+	 {
+		 System.out.print("Odd");
+	 }
+	 else
+	 {
+		 System.out.print("Invalid");
+	 }
+   }
+
+}
