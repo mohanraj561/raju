09bc2a995cf1094e045b0675e8a7880b3b44eadd
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-08 19:13:46 +0530

    Create beg1.java

---
+++ beg1.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class beg1
+{
+   public static void main(String[] args) 
+	{
+		int a=0;
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		if(i>a)
+		{
+			System.out.print("positive");
+		}
+		else if(i<a)
+		{
+			System.out.print("negative");
+		}
+		else
+		{
+			System.out.print("zero");
+		}
+	}
+}
