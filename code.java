109f4d906838c082faead6ea0a971ab0b44adac4
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-09 23:08:06 +0530

    Create code.java

---
+++ code.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+public class summa
+{
+	public static void main(String args[])
+     {
+	    	Scanner sc=new Scanner(System.in);
+	    	int s1=sc.nextInt();
+	      for(int i=0;i<s1;i++)
+	       {
+	        System.out.println("Hello");
+	       }
+     }
+}
+
+
+
+
