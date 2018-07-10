dfe38f9f72379adb100afd237ede6a3badb8b0ea
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-10 06:47:17 +0530

    Create code1.java

---
+++ code1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.util.Scanner;
+public class code1
+{
+	public static void main(String args[])
+     {
+        int count=1;
+        int m=0;
+		Scanner sc=new Scanner(System.in);
+		int s1=sc.nextInt();
+	    while(count<=s1)
+	    {
+	        m=m+count;
+	        count++;
+	        
+	    }
+	    System.out.print(m);
+	    
+     }
+}
+
+
+
+
+
