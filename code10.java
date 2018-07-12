aebf62151fef423166ea3e05b8e18171da16e5c6
mohanraj561 <40682223+mohanraj561@users.noreply.github.com>
2018-07-12 21:55:25 +0530

    Create code10.java

---
+++ code10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.Scanner;
+public class raju 
+{
+   public static void main(String[] args)
+	{
+		int j=0;
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		int k=sc.nextInt();
+	  for(int i=1;i<=5;i++)
+	    {
+	    	j=k*i;
+        System.out.print(j+" ");
+	    }
+	}
+  }
